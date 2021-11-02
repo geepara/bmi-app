5febb87f61cb3108d694793d9c663f55511fdab0
geepara <gpara1697@gmail.com>
2021-11-02 13:01:41 -0400

    Refactor to serve static files

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 7000;
 
+app.use(express.static(path.join(__dirname, "app")));
+
 app.get("/", (request, response) => {
   response.sendFile(path.join(__dirname, "index.html"));
 });
